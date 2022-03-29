968cab49553b6f78176ee48ef6d77d22d6f76554
oskarsus <oskar.staschus@gmx.de>
2022-03-29 10:32:00 +0200

    update player pos

---
+++ core/src/de/pogs/rl/game/background/BackgroundLayer.java
@@ -0,0 +1,13 @@
+package de.pogs.rl.game.background;
+
+public class BackgroundLayer {
+    public BackgroundLayer() {
+
+    }
+
+    public void update() {
+
+    }
+
+    
+}
--- core/src/de/pogs/rl/game/entities/Player.java
+++ core/src/de/pogs/rl/game/entities/Player.java
@@ -1,6 +1,7 @@
 package de.pogs.rl.game.entities;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.ParticleEmitter.SpriteMode;
 import com.badlogic.gdx.math.Vector2;
 
 import org.w3c.dom.Text;
@@ -14,6 +15,8 @@ public class Player extends AbstractEntity {
     private Texture texture = new Texture(Gdx.files.internal("rakete.png"));
     private Sprite sprite;
 
+    private float scale = 0.2f;
+
     private float angle = 0;
     private float aimedAngle = 0;
 
@@ -21,7 +24,8 @@ public class Player extends AbstractEntity {
 
     public Player() {
         sprite = new Sprite(texture);
-        sprite.setScale(0.2f);
+        sprite.setSize(texture.getWidth() * scale, texture.getHeight() * scale);
+        sprite.setOrigin(sprite.getWidth() / 2, sprite.getHeight() / 2);
         position.set(0, 0);
     }
 
@@ -33,14 +37,10 @@ public class Player extends AbstractEntity {
     @Override
     public void update(float delta, Input input) {
         updateAimedAngle();
-
-        sprite.setPosition(position.x - (texture.getWidth() / 2), position.y + texture.getHeight() * sprite.getScaleY() / 2);
+        System.out.println( sprite.getWidth());
+        sprite.setPosition(position.x - (sprite.getWidth() / 2), position.y - sprite.getHeight() / 2);
         sprite.setRotation(aimedAngle);
-        // if (input.isKeyPressed(Keys.LEFT)) sprite.setPosition(sprite.getX() - speed *
-        // delta, sprite.getY());
 
-        // if (input.isKeyPressed(Keys.RIGHT)) sprite.setPosition(sprite.getX() + speed
-        // * delta, sprite.getY());
     }
 
     private void updateAimedAngle() {
