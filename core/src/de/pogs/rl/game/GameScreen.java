bf5f4fc6d904d765ad5b27db8f83ed4ca17e1e4c
phgilde <ph.gilde@gmail.com>
2022-04-05 10:29:23 +0200

    remove obsolete imports

--- core/src/de/pogs/rl/game/GameScreen.java
+++ core/src/de/pogs/rl/game/GameScreen.java
@@ -5,7 +5,6 @@ import java.util.LinkedList;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import de.pogs.rl.RocketLauncher;
--- core/src/de/pogs/rl/game/entities/Player.java
+++ core/src/de/pogs/rl/game/entities/Player.java
@@ -6,8 +6,6 @@ import com.badlogic.gdx.math.Vector2;
 import de.pogs.rl.utils.SpecialMath;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
