6c51aba2c2a9f1ea4e4a29d8d6f08df18e8b3b93
pgmcr <ph.gilde@gmail.com>
2022-05-29 14:32:12 +0200

    tank enemy texture

--- core/src/de/pogs/rl/game/world/entities/TankEnemy.java
+++ core/src/de/pogs/rl/game/world/entities/TankEnemy.java
@@ -6,17 +6,28 @@ import de.pogs.rl.RocketLauncher;
 import de.pogs.rl.utils.SpecialMath.Vector2;
 
 public class TankEnemy extends SimpleEnemy {
-    
-    private Texture texture = RocketLauncher.INSTANCE.assetHelper.getImage("monster2");
+
+    private float hp = 10;
+
     public TankEnemy(Vector2 position) {
-        
-        super(position);
-        sprite.setTexture(texture);
+        super(position, RocketLauncher.INSTANCE.assetHelper.getImage("monster2"));
         shootingCoeff = 10f;
-        bulletSpeed = 1000;
-        bulletDamage = 10;
+        bulletSpeed = 5000;
+        bulletDamage = 0;
+        bulletColor = new java.awt.Color(30, 255, 30, 255);
     }
+
     public TankEnemy(float x, float y) {
         this(new Vector2(x, y));
+    }
+
+    @Override
+    public void addDamage(float damage, AbstractEntity source) {
+        hp -= damage;
+        if (hp < 0) {
+            this.alive = false;
+            source.killOtherEvent(this);
+            splashEffectSelf();
+        }
     }
 }
