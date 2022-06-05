74f63c762646e2826224d0bc72b08821e4234f55
oskar2594 <oskar.stanschus@gmx.de>
2022-06-05 22:56:25 +0200

    fix permanent saving

--- core/src/de/pogs/rl/game/PlayerStats.java
+++ core/src/de/pogs/rl/game/PlayerStats.java
@@ -47,7 +47,6 @@ public class PlayerStats {
         }
         updateLevelData();
         ConfigLoader.setValue(highscore, "highscore");
-        ConfigLoader.save();
     }
 
     public static void saveHighscore() {
